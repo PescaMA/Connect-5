07c7764fcffca399b63eb4b43f85cc53709f5125
PescaMA <105070516+PescaMA@users.noreply.github.com>
2022-05-06 23:53:52 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,32 +2,18 @@
 #include <string.h>;
 using namespace std;
 short board[105][105];
-short n,m;
-bool safe=true;
+short n,m,safe;
+#pragma region
 void SetBoard()
 {
     for(int i=1;i<=n;i++)
-        board[i][0]=1,board[i][m+1]=2;
+        board[i][0]=board[i][m+1]=1;
     for(int j=1;j<=m;j++)
-        board[0][j]=3,board[n+1][j]=4;
-    board[0][0]=5;
-    board[n+1][0]=6;
-    board[0][m+1]=7;
-    board[n+1][m+1]=8;
-}
-void ShowBoard()
-{
-    char ch[69];
-    if(safe)
-        strcpy(ch," ||==++++XO");
-    else ///       0123456789
-        strcpy(ch,"sup");
-    for(int i=0;i<=n+1;i++)
-    {
-        for(int j=0;j<=m+1;j++)
-            cout<<ch[ board[i][j] ];
-        cout<<'\n';
-    }
+        board[0][j]=board[n+1][j]=2;
+    board[0][0]=3;
+    board[n+1][0]=4;
+    board[0][m+1]=5;
+    board[n+1][m+1]=6;
 }
 void CClear()
 { /// clears console
@@ -37,12 +23,35 @@ void CClear()
         std::system ("clear");
     #endif
 }
-template <typename T> void ReadNR(T &x)
+void ShowBoard()
 {
-    string error;
-    while(!(cin>>x) || (x<1 || x>99))
+    CClear();
+    char ch[69];
+    if(safe)
+        strcpy(ch," |=++++XO#");
+    else
+    {   /// it only works when converting to char (like here)
+        int v[]={32,186,205,201,200,187,188,5,232,177};
+        /// is  [spc]║   ═   ╔   ╚    ╗   ╝ ♣  Φ   ▒
+        for(int i=0;i<10;i++)
+            ch[i]=v[i];
+        ch[10]='\0';
+    }
+
+    for(int i=0;i<=n+1;i++)
     {
-        cout<<"Valoarea trebuie sa fie numar 1-99, incercati din nou: ";
+        for(int j=0;j<=m+1;j++)
+            cout<<ch[ board[i][j] ];
+        cout<<'\n';
+    }
+}
+template <class T,class S,class TT,class TTT>
+void ReadNR(T &x ,S tryy, TT mn, TTT mx)
+{ /// very normal function with no excesive complications
+    string error;
+    while(!(cin>>x) || (x<mn || x>mx))
+    {
+        cout<<tryy;
         if(cin.fail())
         {
             cin.clear();
@@ -51,18 +60,123 @@ template <typename T> void ReadNR(T &x)
     }
     if(cin.peek()!=' ' && cin.peek()!='\n')
         cin>>error;
-    CClear();
 }
 void SetSize()
 {
+    string unu100="Valoarea trebuie sa fie numar 1-99, incercati din nou: ";
+    string zero1="Valoarea trebuie sa fie 1(da) sau 0, incercati din nou: ";
     cout<<"Introduceti inaltimea tablei ";
-    ReadNR(n);
+    ReadNR(n,unu100,1,99);
+    CClear();
     cout<<"Introduceti latimea tablei ";
-    ReadNR(m);
+    ReadNR(m,unu100,1,99);
+    CClear();
+    cout<<"Vreti grafica simpla, dar 100% functionala(0 sau 1)? ";
+    ReadNR(safe,zero1,0,1);
     SetBoard();
+}
+#pragma endregion
+int scor1,scor2;
+int const scormx=50;
+bool finished()
+{
+    if(scor1>scormx || scor2>scormx)
+        return true;
+    for(int j=1;j<=m;j++)
+        if(!board[1][j])
+            return false;
+    return true;
+}
+void ending()
+{
+    cout<<"Scorul tau este "<<scor1;
+    cout<<"\nScorul AI-ului este: "<<scor2<<'\n';
+    if(scor1>scor2)
+        cout<<"Ai castigat!";
+    else if(scor1<scor2)
+        cout<<"Ai-ul a castigat!";
+    else
+        cout<<"Egalitate!";
+}
+bool legal(int col)
+{
+    return board[1][col]==0;
+}
+int c;
+void dfs(int x,int y,int nr,int nr2,int cmx)
+{
+    if(board[x][y]!=nr || c==cmx)
+        return;
+    c++;
+    board[x][y]=nr2;
+    dfs(x+1,y,nr,nr2,cmx);dfs(x,y+1,nr,nr2,cmx);
+    dfs(x-1,y,nr,nr2,cmx);dfs(x,y-1,nr,nr2,cmx);
+}
+bool cinci(int x,int y,int nr)
+{
+    c=0;
+    bool pp=false;
+    dfs(x,y,nr,100,5);
+    if(c==5)
+        pp=true;
+    dfs(x,y,100,nr,5);
+    return pp;
+}
+void movee(int col,int nr)
+{
+    int lin;
+    for(lin=1;lin<=n && !board[lin][col];lin++);
+    lin--;
+    board[lin][col]=nr+6;
+    if(cinci(lin,col,nr+6))
+    {
+        c=0;
+        dfs(lin,col,nr+6,1,10000);
+        if(nr==1)
+            scor1+=c;
+        else
+            scor2+=c;
+    }
+}
+void AI_move()
+{
+    int rnd=rand()%m+1;
+    if(legal(rnd))
+    {
+        movee(rnd,2);
+        return;
+    }
+    AI_move();
+}
+void Play()
+{
+    while(true)
+    {
+        ShowBoard();
+        if(finished())
+            return ending();
+        cout<<"Pe ce coloana doresti sa pui: ";
+        int col;
+        string invalid="Valoare invalida, ziceti un nr de col libera: ";
+        while(true)
+        {
+            ReadNR(col,invalid,1,m);
+            if(legal(col))
+            {
+                movee(col,1);
+                break;
+            }
+            else
+                cout<<invalid;
+        }
+        if(finished())
+            return ending();
+        AI_move();
+    }
 }
 int main()
 {
     SetSize();
-    ShowBoard();
+    Play();
+
 }
